45f7329130758de735fe380017fe33a5f0f20441
qianchengjie <330616153@qq.com>
2017-04-29 16:31:40 +0800

    增加未读变已读ajax

--- js/message.js
+++ js/message.js
@@ -3,9 +3,22 @@ $(document).ready(function() {
 	$('.message-content li').click(function() {
 		$(this).addClass('already-read');
 		var tds = $(this).find('td');
-		var message = $(this).find("[type='hidden']").val()
+		var message = $(this).find('.message-detailed').val()
+		var msg_id = $(this).find('.msg-id').val()
 		$('.dialog-content').find('p').text(message);
 		$('.dialog').show();
+
+		//变成已读ajax
+		$.ajax({
+			url : '',
+			type : 'POST',
+			date : {
+				msgID : msg_id
+			},
+			success : function(date){
+
+			}
+		})
 	})
 	$('.all-read').click(function(){
 		$('.message-content').find('li').addClass('already-read')
