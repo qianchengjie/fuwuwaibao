a232e424f09ca3a8ac0c7e35e176d930920a450a
qianchengjie <330616153@qq.com>
2017-04-29 15:27:20 +0800

    新增接收消息页面

---
+++ js/message.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$('.message-content li').click(function() {
+		$(this).addClass('already-read');
+		var tds = $(this).find('td');
+		var message = $(this).find("[type='hidden']").val()
+		$('.dialog-content').find('p').text(message);
+		$('.dialog').show();
+	})
+	$('.all-read').click(function(){
+		$('.message-content').find('li').addClass('already-read')
+	})
+
+});
